3c20f804510cca33a05fb1990bada7e1a74e6cbe
Douglas Moura <douglas.ademoura@gmail.com>
2024-07-24 13:34:44 -0300

    feat: add Storybook theming plugin

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react'
+import { themes } from '@storybook/theming';
 import '../src/styles/global.css'
 
 const preview: Preview = {
@@ -8,6 +9,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+      docs: {
+        theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? themes.dark : themes.light
+      }
     },
   },
 }
