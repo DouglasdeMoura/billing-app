2fd1b3841363b661f840bd5c47fa40dd5e8a1012
Douglas Moura <douglas.ademoura@gmail.com>
2024-07-24 13:34:10 -0300

    chore: categorize the component

--- src/components/title/title.stories.tsx
+++ src/components/title/title.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Title } from '.'
 
 export default {
-  title: 'Title',
+  title: 'components/Title',
   component: Title,
 } as Meta<typeof Title>
 
