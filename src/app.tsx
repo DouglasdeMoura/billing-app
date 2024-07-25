6639a178f1d0cb880cf39434d85920230f2ae6e9
Douglas Moura <douglas.ademoura@gmail.com>
2024-07-24 22:48:47 -0300

    style: organize the fields

--- src/app.tsx
+++ src/app.tsx
@@ -1,37 +1,50 @@
 import { Form } from '~/components/form'
+import { NumberField } from '~/components/number-field'
 import { Select } from '~/components/select'
 import { TextField } from '~/components/text-field'
 import { Title } from '~/components/title'
 
+const Line: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
+  <div className="grid grid-cols-1 md:grid-cols-3 gap-4">{children}</div>
+)
+
+const TwoInputs: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return <div className="grid grid-cols-2 gap-2 items-end">{children}</div>
+}
+
 export const App: React.FC = () => {
   return (
-    <section className="m-auto max-w-screen-lg p-4">
-      <header>
+    <section className="m-auto container p-4">
+      <header className="mb-8">
         <Title className="text-center">Setup your subscription</Title>
       </header>
       <Form>
-        <TextField label="Initial Price" />
+        <Line>
+          <TextField label="Initial Price" />
 
-        <div className="flex">
-          <TextField label="Billing frequency" />
-          <Select options={[{ id: 'months', children: 'Months' }]} />
-        </div>
+          <TwoInputs>
+            <NumberField label="Billing frequency" />
+            <Select options={[{ id: 'months', children: 'Months' }]} />
+          </TwoInputs>
 
-        <TextField label="Monthly payment" />
+          <TextField label="Monthly payment" />
+        </Line>
 
-        <div>
-          <TextField label="Trial period" />
-          <Select
-            options={[
-              { id: 'days', children: 'days' },
-              { id: 'weeks', children: 'Weeks' },
-            ]}
-          />
-        </div>
+        <Line>
+          <TwoInputs>
+            <NumberField label="Trial period" />
+            <Select
+              options={[
+                { id: 'days', children: 'days' },
+                { id: 'weeks', children: 'Weeks' },
+              ]}
+            />
+          </TwoInputs>
 
-        <TextField label="Duration" />
+          <TextField label="Duration" />
 
-        <TextField label="Billing Cycles" />
+          <TextField label="Billing Cycles" />
+        </Line>
       </Form>
       <div>Your customer will be charged</div>
     </section>
--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -8,7 +8,7 @@ export const Title: React.FC<TitleProps> = ({ children, ...props }) => (
   <Heading
     {...props}
     slot="title"
-    className={`inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 ${props?.className ? props.className : ''}`}
+    className={`text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-slate-200 ${props?.className ? props.className : ''}`}
   >
     {children}
   </Heading>
