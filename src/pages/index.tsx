ef9966f3acd3e6552bc18d74489efaf81f80c36a
Douglas Moura <douglas.ademoura@gmail.com>
2024-07-24 23:34:14 -0300

    feat: add controlled field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from 'react-router-dom'
+
 import { Form } from '~/components/form'
 import { NumberField } from '~/components/number-field'
 import { Select } from '~/components/select'
@@ -12,6 +14,78 @@ const TwoInputs: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return <div className="grid grid-cols-2 gap-2 items-end">{children}</div>
 }
 
+type ControlledFieldProps = {
+  label?: string
+  'aria-label'?: string
+  name: string
+  options?: { id: string; children: string }[]
+  type?: 'number' | 'text'
+}
+
+const ControlledField: React.FC<ControlledFieldProps> = ({
+  label,
+  name,
+  type,
+  'aria-label': ariaLabel,
+  options,
+}) => {
+  const [searchParams, setSearchParam] = useSearchParams()
+
+  const handleChange = (value: string | number) => {
+    // @ts-expect-error - TS is infering the wrong type for the state
+    setSearchParam((state) => {
+      const object = Object.fromEntries(state)
+
+      if (!value) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { [name]: _, ...rest } = object
+        return rest
+      }
+
+      return { ...object, [name]: value }
+    })
+  }
+
+  if (type === 'number') {
+    const value = searchParams.get(name) ? +searchParams.get(name)! : undefined
+
+    return (
+      <NumberField
+        aria-label={ariaLabel}
+        label={label}
+        name={name}
+        defaultValue={value}
+        onChange={handleChange}
+        minValue={0}
+      />
+    )
+  }
+
+  const value = searchParams.get(name) ? searchParams.get(name)! : undefined
+
+  if (options) {
+    return (
+      <Select
+        aria-label={ariaLabel}
+        options={options}
+        name={name}
+        defaultSelectedKey={value}
+        onSelectionChange={handleChange}
+      />
+    )
+  }
+
+  return (
+    <TextField
+      aria-label={ariaLabel}
+      label={label}
+      name={name}
+      defaultValue={value}
+      onChange={handleChange}
+    />
+  )
+}
+
 export const App: React.FC = () => {
   return (
     <section className="m-auto container p-4">
@@ -20,11 +94,23 @@ export const App: React.FC = () => {
       </header>
       <Form>
         <Line>
-          <TextField label="Initial Price" />
+          <ControlledField
+            label="Initial Price"
+            name="initialPrice"
+            type="number"
+          />
 
           <TwoInputs>
-            <NumberField label="Billing frequency" />
-            <Select options={[{ id: 'months', children: 'Months' }]} />
+            <ControlledField
+              label="Billing frequency"
+              name="billingFrequency"
+              type="number"
+            />
+            <ControlledField
+              aria-label="Select the billing frequency"
+              options={[{ id: 'months', children: 'Months' }]}
+              name="frequency"
+            />
           </TwoInputs>
 
           <TextField label="Monthly payment" />
@@ -32,18 +118,24 @@ export const App: React.FC = () => {
 
         <Line>
           <TwoInputs>
-            <NumberField label="Trial period" />
-            <Select
+            <ControlledField
+              label="Trial period"
+              name="trialPeriod"
+              type="number"
+            />
+            <ControlledField
+              aria-label="Select the trial period"
               options={[
-                { id: 'days', children: 'days' },
+                { id: 'days', children: 'Days' },
                 { id: 'weeks', children: 'Weeks' },
               ]}
+              name="period"
             />
           </TwoInputs>
 
-          <TextField label="Duration" />
+          <ControlledField label="Duration" name="duration" />
 
-          <TextField label="Billing Cycles" />
+          <ControlledField label="Billing Cycles" name="billingCycles" />
         </Line>
       </Form>
       <div>Your customer will be charged</div>
